393068a684bf1480f06b80ea4bdeabac6c5b0e63
Getkey <getkey@gmx.fr>
2016-07-20 11:52:06 +0200

    Fix previous commit and add a few niceties

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,15 +1,19 @@
-function fish_prompt
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
+function fish_prompt -d 'Write out the left prompt'
+	set last_status $status
+
+	if test $USER = 'root' > /dev/null
+		set fmtd_user (set_color -o red) $USER
+		set suffix '# '
+	else
+		set fmtd_user (set_color -o green) $USER
+		set suffix '> '
 	end
 
-	switch $USER
-	
-		case root			
-			set __fish_prompt_cwd (set_color -o $fish_color_cwd_root)		
-			printf "%s%s #%s " $__fish_prompt_cwd (prompt_pwd) (set_color normal)
-		case '*'
-			printf "%s%s >%s " $__fish_prompt_cwd (prompt_pwd) (set_color normal)
-
+	if test $last_status = '0' > /dev/null
+		set prompt_sign (set_color green) $suffix
+	else
+		set prompt_sign (set_color red) $suffix
 	end
+
+	echo -n -s $fmtd_user (set_color normal) @ (set_color -o green) (hostname) (set_color blue) ' ' (prompt_pwd) (set_color -o green) $prompt_sign (set_color normal)
 end
--- .vim/vundle-loader.vim
+++ .vim/vundle-loader.vim
@@ -14,6 +14,7 @@ endif
 Plugin 'sheerun/vim-polyglot'
 let g:rust_recommended_style = 0
 Plugin 'Raimondi/delimitMate'
+Plugin 'bolasblack/csslint.vim'
 Plugin 'airblade/vim-gitgutter'
 set updatetime=250
 Plugin 'tpope/vim-vinegar'
