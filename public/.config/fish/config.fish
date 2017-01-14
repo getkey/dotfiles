3d78bf0a273bcafe86dd77cd46e791c6cdbc0ce5
Getkey <getkey@getkey.eu>
2017-01-14 12:09:02 +0100

    Set CLI locale to english
    So I can serch for debug messages on the Internet without having to guess what the english translation is.
    The GUI locale can be set independently.

--- public/.config/fish/config.fish
+++ public/.config/fish/config.fish
@@ -2,3 +2,6 @@ set fish_greeting # Disable greeting
 
 set PATH $HOME/.cargo/bin $PATH
 set -x OLIMEX 'getkey@getkey.eu'
+
+set -x LC_MESSAGES 'en_US.UTF-8' # for CLI programs
+# another language can be used for GUI programs by exporting in ~/.xinitrc, ie export LC_MESSAGES=fr_FR.UTF-8
