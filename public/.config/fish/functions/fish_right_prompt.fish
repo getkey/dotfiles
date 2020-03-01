9f9a7a75eaf554dc08e0456dd8d7a3e89d7daf84
Julien Mourer <getkey@getkey.eu>
2020-03-01 20:20:46 +0100

    Remove deprecated operator in Fish 3.1
    See https://github.com/fish-shell/fish-shell/issues/4394

--- public/.config/fish/functions/fish_right_prompt.fish
+++ public/.config/fish/functions/fish_right_prompt.fish
@@ -15,7 +15,7 @@ end
 
 function fish_right_prompt
 	if not set -q __git_cb
-		if test (git rev-parse --is-inside-work-tree 2> /dev/null ^&1 | tail -n 1) = 'true'
+		if test (git rev-parse --is-inside-work-tree 2> /dev/null 2>&1 | tail -n 1) = 'true'
 			set __git_cb (parse_git_dirty)'('(git branch --list | grep '\*' | sed 's/\*\ //')(parse_git_changes)')'(set_color normal)
 		else
 			set __git_cb ''
