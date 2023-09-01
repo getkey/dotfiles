8302874f13da7ab21058d3e9f5795cade6ed1762
Julien Mourer <getkey@getkey.eu>
2023-09-02 00:53:55 +0200

    Upgrade stateVersion
    I was getting errors that docker and podman don't get along with this stateVersion.
    Actually, I don't even need Docker, so I removed it.

--- machines/hus/configuration.nix
+++ machines/hus/configuration.nix
@@ -66,7 +66,6 @@
 			"networkmanager"
 			"adbusers"
 			"dialout" # access serial ports for Arduino
-			"docker"
 		];
 		shell = pkgs.fish;
 	};
@@ -76,7 +75,6 @@
 	programs.adb.enable = true;
 	services.gvfs.enable = true;
 
-	virtualisation.docker.enable = true;
 	virtualisation.podman.enable = true;
 
 	# used by redshift
@@ -96,9 +94,9 @@
 
 	# This value determines the NixOS release from which the default
 	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# on your system were taken. It's perfectly fine and recommended to leave
 	# this value at the release version of the first install of this system.
 	# Before changing this value read the documentation for this option
 	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "20.09"; # Did you read the comment?
+	system.stateVersion = "23.05"; # Did you read the comment?
 }
