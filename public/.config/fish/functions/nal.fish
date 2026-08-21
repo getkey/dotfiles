23c87e888cbb685ab9ae9d1ad16438a5a5839d37
Julien Mourer <getkey@getkey.eu>
2026-08-21 11:26:00 +0200

    Do not attempt to remount the journal if it's already mounted

--- public/.config/fish/functions/nal.fish
+++ public/.config/fish/functions/nal.fish
@@ -1,5 +1,7 @@
 function nal
-	gocryptfs $NAL_PATH ~/mnt/; or return
+	if not mount | string match -q "* on $HOME/mnt (*"
+		gocryptfs $NAL_PATH ~/mnt/; or return
+	end
 	read -P 'Where are you? ' location
 	printf '\n\n## '(date '+%F %R')', '$location'\n\n' >> ~/mnt/journal/journal.md
 	$EDITOR '+normal Go' +startinsert ~/mnt/journal/journal.md
